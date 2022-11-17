7c4cb042074cdb7f9d094019b8c1579156e3a388
Orestis Meikopoulos <ormeikop@microsoft.com>
2022-11-17 19:18:17 +0200

    Minor changes

--- End/ReGeneration.PaymentGateway.Api/Controllers/ApiControllerBase.cs
+++ End/ReGeneration.PaymentGateway.Api/Controllers/ApiControllerBase.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ReGeneration.PaymentGateway.Api.Application.Common.Interfaces;
 
 namespace ReGeneration.PaymentGateway.Api.Controllers
